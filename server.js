a5a5ad5bb925a16c11af0b6a1a143ad1811bc5a9
Ivan Nefagin <dfyz0r@list.ru>
2021-01-04 13:25:15 +0300

    feat(server.js): add POST-request handling

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
   cors: {
@@ -10,8 +11,24 @@ const io = require('socket.io')(server, {
 
 const roomDatabase = new Map();
 
+// Allows app to receive json data
+app.use(express.json());
+
 app.get('/rooms', (request, response) => {
   response.json(roomDatabase);
+});
+
+app.post('/rooms', (request, response) => {
+  const { roomId, username } = request.body;
+
+  if (!roomDatabase.has(roomId)) {
+    roomDatabase.set(roomId, new Map([
+      ['users', new Map()],
+      ['messages', []],
+    ]));
+  }
+
+  response.send(); // send response status 200
 });
 
 io.of('/').use((socket, next) => {
