06e2add3ecaff850f94e026b665ba38e8bd3f225
Ivan Nefagin <dfyz0r@list.ru>
2021-01-04 20:01:33 +0300

    feat(App.js): add new props to state, add GET-request onLogin, pass users and messages to Chat component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useReducer, useEffect } from 'react';
+import axios from 'axios';
 
 import socket from './socket';
 import reducer from './reducer';
@@ -10,28 +11,39 @@ const App = () => {
   const [state, dispatch] = useReducer(reducer, {
     joined: false,
     roomId: null,
-    username: null
+    username: null,
+    users: [],
+    messages: []
   });
 
-  const onLogin = (data) => {
+  const onLogin = async data => {
     dispatch({
       type: 'JOINED',
       payload: data
     });
 
     socket.emit('ROOM:JOIN', data);
+    const { data: responseData } = await axios.get(`/rooms/${data.roomId}`)
+      .catch(err => console.error(err));
+
+    setUsers(responseData.users);
+  };
+
+  const setUsers = users => {
+    dispatch({
+      type: 'SET_USERS',
+      payload: users
+    });
   };
 
   useEffect(() => {
-    socket.on('ROOM:JOINED', users => {
-      console.log('new user', users)
-    });
+    socket.on('ROOM:SET_USERS', setUsers);
   }, []);
 
   return (
     <div className="App">
       <div className="wrapper">
-        {!state.joined ? <Join onLogin={onLogin} /> : <Chat />}
+        {!state.joined ? <Join onLogin={onLogin} /> : <Chat users={state.users} messages={state.messages} />}
       </div>
     </div>
   );
