728097510f369265ffa6ba18343411557a3ccca5
Ivan Nefagin <dfyz0r@list.ru>
2021-01-04 12:23:06 +0300

    feat(Join.jsx): add states and onChange handlers of inputs

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -1,15 +1,20 @@
+import { useState } from 'react';
+
 import socket from '../socket';
 
 const Join = () => {
+  const [roomId, setRoomId] = useState('');
+  const [username, setUsername] = useState('');
+
   return (
     <div className="join">
       <label className="join__label">
         ID комнаты
-        <input className="join__input" type="text" value="" placeholder="4444"/>
+        <input className="join__input" type="text" value={roomId} onChange={evt => setRoomId(evt.target.value)} placeholder="4444"/>
       </label>
       <label className="join__label">
         Никнейм
-        <input className="join__input" type="text" value="" placeholder="gsk"/>
+        <input className="join__input" type="text" value={username} onChange={evt => setUsername(evt.target.value)} placeholder="gsk"/>
       </label>
       <button className="join__button button button--green">Войти</button>
     </div>
